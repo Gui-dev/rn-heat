324b844da4067b41223d8fb2a29e6e027b259d0d
Gui-dev <adrianon10@gmail.com>
2022-04-02 03:03:18 -0300

    feat: add checking user logged

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 
 import { Header } from '../../components/Header'
 import { MessagesList } from '../../components/MessagesList'
+import { SendMessageForm } from '../../components/SendMessageForm'
 import { SignInBox } from '../../components/SignInBox'
+import { useAuth } from '../../hooks/useAuth'
 
 import { Container } from './style'
 
 export const Home = () => {
+  const { user } = useAuth()
   return (
     <Container>
       <Header />
       <MessagesList />
-      <SignInBox />
+      { user ? <SendMessageForm /> : <SignInBox /> }
     </Container>
   )
 }
