b8299face082f4d108a7576d2d96fd678f0f3014
Gui-dev <adrianon10@gmail.com>
2022-04-02 03:31:06 -0300

    feat: add messages in real time

--- src/components/MessagesList/index.tsx
+++ src/components/MessagesList/index.tsx
@@ -1,23 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
 
-import { Message } from '../Message'
+import { api } from '../../services/api'
+import { Message, MessageProps } from '../Message'
 import { Container } from './style'
 
 export const MessagesList = () => {
-  const message = {
-    id: '1',
-    text: 'Teste de mensagem',
-    user: {
-      name: 'Gui Silva',
-      avatar_url: 'https://github.com/Gui-dev.png'
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([])
+
+  const socket = io(String(api.defaults.baseURL))
+  const messagesQueue: MessageProps[] = []
+
+  socket.on('new_message', (message) => {
+    messagesQueue.push(message)
+    console.log(message)
+  })
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const { data } = await api.get<MessageProps[]>('/messages/last-messages')
+      setCurrentMessages(data)
     }
-  }
+
+    fetchMessages()
+  }, [])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setCurrentMessages(prevState => [messagesQueue[0], prevState[0], prevState[1]])
+        messagesQueue.shift()
+      }
+    }, 3000)
+
+    return () => clearInterval(timer)
+  }, [messagesQueue])
 
   return (
     <Container
       keyboardShouldPersistTaps="never"
     >
-      <Message data={ message }/>
+      {currentMessages.map(message => {
+        return (
+          <Message key={message.id} data={ message }/>
+        )
+      })}
     </Container>
   )
 }
